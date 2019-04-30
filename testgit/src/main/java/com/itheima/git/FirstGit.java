310051cf7c15df746e9f2c9d1e395b616f7a3169
zby <zby@itcast.cn>
2019-04-30 14:29:47 +0800

    bendi

--- testgit/src/main/java/com/itheima/git/FirstGit.java
+++ testgit/src/main/java/com/itheima/git/FirstGit.java
@@ -12,5 +12,10 @@ public class FirstGit {
         System.out.println("hello");
         System.out.println("hello");
         System.out.println("hello");
+
+    }
+
+    public void test(){
+        int i = 0;
     }
 }
